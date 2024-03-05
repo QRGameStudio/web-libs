4d63e1192a8843c943b02cbd3ac2f9ed15c47151
Adam Hlaváček <git@adamhlavacek.com>
2024-03-05 15:17:19 +0100

    feat: saving asteroids

--- GUtils/GUt.js
+++ GUtils/GUt.js
@@ -129,4 +129,15 @@ class GUt {
         console.debug('[GUt] Screen size', width, height);
         return width > height;
     }
+
+    /**
+     * Generates UUIDv4
+     * @return {string} uuid
+     */
+    static uuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
 }
