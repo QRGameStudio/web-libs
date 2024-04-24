cc9f2f7ca7cb929aaf0bf6914bc5519bf2498688
Adam Hlaváček <git@adamhlavacek.com>
2024-04-24 16:01:22 +0200

    feat(popup): stack

--- GPopup/GPopup.js
+++ GPopup/GPopup.js
@@ -7,11 +7,21 @@
 function GPopup(content, duration = null) {
     const el = document.createElement('div');
     el.classList.add('popup');
+    el.classList.add('qr-popup');
     el.classList.add('btn');
     el.classList.add('btn-lg');
     el.classList.add('btn-info');
     el.innerHTML = content;
+    el.dataset.firedAt = `${Date.now()}`;
+    const observer = new MutationObserver(() => positionItself());
     let shown = false;
+
+    function positionItself() {
+        const popupBefore = [...document.querySelectorAll('.qr-popup')]
+            .filter(e => Number(e.dataset.firedAt) < Number(el.dataset.firedAt))
+            .sort((a, b) => Number(b.dataset.firedAt) - Number(a.dataset.firedAt))[0];
+        el.style.top = popupBefore ? `${popupBefore.getBoundingClientRect().bottom + 10}px` : '';
+    }
 
     /**
      * Shows the popup to the screen
@@ -21,6 +31,7 @@ function GPopup(content, duration = null) {
         if (shown)
             return;
         shown = true;
+        observer.observe(document.body, {childList: true, subtree: false, attributes: false, characterData: false});
         document.body.appendChild(el);
         el.classList.add('popup-in');
         el.addEventListener('click', () => this.remove().then());
@@ -40,6 +51,7 @@ function GPopup(content, duration = null) {
                 return;
             }
             shown = false;
+            observer.disconnect();
             el.classList.remove('popup-in');
             el.classList.add('popup-out');
             setTimeout(() => {
